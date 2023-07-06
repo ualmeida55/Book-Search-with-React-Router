772316a3ea01d8ae7589a1e3767acee3b5c3bae2
ualmeida55 <ualmeida55@gmail.com>
2023-07-06 18:52:40 -0300

    Interface modifications

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/searchResults" element={<SearchResults />}></Route>
+    </Routes>
+  </Router>
+);
+
+function Home() {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col-xxl-11 gx-2">
+          <label htmlFor=""></label>
+          <input
+            type="text"
+            class="form-control bg-dark text-white"
+            placeholder="Type book name"
+          />
+        </div>
+        <div class="col-xxl-1 searchButton">
+          <button class="btn btn-primary">Search</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+function SearchResults() {
+  return (
+    <div>
+      <input type="text" id="search-bar" placeholder="Type Book" />
+      <button type="button" class="btn btn-warning">
+        Warning
+      </button>
+      <h1>aaaa</h1>
+    </div>
+  );
+}
