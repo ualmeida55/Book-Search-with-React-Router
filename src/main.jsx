590047d1db2b71ced43b540533e7f5ddec40046e
ualmeida55 <ualmeida55@gmail.com>
2023-07-10 21:43:38 -0300

    MVP commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/searchResults" element={<SearchResults />}></Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/searchResults" element={<SearchResults />} />
     </Routes>
   </Router>
 );
 
 function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = () => {
+    setIsLoading(true);
+    const searchTerm = document.getElementById("search-bar").value;
+
+    fetch(`https://openlibrary.org/search.json?q=${searchTerm}`)
+      .then(response => response.json())
+      .then(data => {
+        setIsLoading(false);
+        setSearchResults(data.docs);
+      })
+      .catch(error => {
+        console.error("Error fetching search results:", error);
+        setIsLoading(false);
+      });
+  };
+
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col-xxl-11 gx-2">
+    <div className="container">
+      <div className="row">
+        <div className="col-xxl-11 gx-2">
           <label htmlFor=""></label>
           <input
             type="text"
-            class="form-control bg-dark text-white"
+            className="form-control bg-dark text-white"
             placeholder="Type book name"
+            id="search-bar"
           />
         </div>
-        <div class="col-xxl-1 searchButton">
-          <button class="btn btn-primary">Search</button>
+        <div className="col-xxl-1 searchButton">
+          <button className="btn btn-primary" onClick={handleSearch}>
+            Search
+          </button>
         </div>
       </div>
+
+      {isLoading && <p>Loading...</p>}
+
+      {!isLoading && searchResults.length > 0 && (
+        <div>
+          <h1>Search Results</h1>
+          {searchResults.map(result => (
+            <div key={result.key}>
+              <img
+                src={`https://covers.openlibrary.org/b/olid/${result.cover_edition_key}-M.jpg`}
+                alt="Book Cover"
+              />
+              <p>{result.title}</p>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {!isLoading && searchResults.length === 0 && (
+        <p>No results found.</p>
+      )}
     </div>
   );
 }
+
 function SearchResults() {
   return (
     <div>
-      <input type="text" id="search-bar" placeholder="Type Book" />
-      <button type="button" class="btn btn-warning">
-        Warning
-      </button>
-      <h1>aaaa</h1>
+      <h1>Search Results Page</h1>
     </div>
   );
 }
